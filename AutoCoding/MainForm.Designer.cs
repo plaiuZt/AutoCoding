811bc169302950cee9a26ebe1984ab23702e6535
PlaiuZt <511549108@qq.com>
2018-07-06 23:28:57 +0800

    配置页面

--- AutoCoding/MainForm.Designer.cs
+++ AutoCoding/MainForm.Designer.cs
@@ -28,44 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnTop = new System.Windows.Forms.Panel();
+            this.lbConnStr = new System.Windows.Forms.Label();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.tbConnStr = new System.Windows.Forms.TextBox();
+            this.pnBottom = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gbCenter = new System.Windows.Forms.GroupBox();
+            this.pnTitle = new System.Windows.Forms.Panel();
+            this.spContent = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDb = new System.Windows.Forms.ComboBox();
+            this.lvTable = new System.Windows.Forms.ListView();
+            this.TableName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.pnTop.SuspendLayout();
+            this.pnBottom.SuspendLayout();
+            this.gbCenter.SuspendLayout();
+            this.pnTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spContent)).BeginInit();
+            this.spContent.Panel1.SuspendLayout();
+            this.spContent.Panel2.SuspendLayout();
+            this.spContent.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnTop
+            // 
+            this.pnTop.Controls.Add(this.lbConnStr);
+            this.pnTop.Controls.Add(this.btnConn);
+            this.pnTop.Controls.Add(this.tbConnStr);
+            this.pnTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTop.Location = new System.Drawing.Point(0, 0);
+            this.pnTop.Name = "pnTop";
+            this.pnTop.Size = new System.Drawing.Size(1029, 40);
+            this.pnTop.TabIndex = 1;
+            // 
+            // lbConnStr
+            // 
+            this.lbConnStr.AutoSize = true;
+            this.lbConnStr.Location = new System.Drawing.Point(19, 13);
+            this.lbConnStr.Name = "lbConnStr";
+            this.lbConnStr.Size = new System.Drawing.Size(65, 12);
+            this.lbConnStr.TabIndex = 6;
+            this.lbConnStr.Text = "数据库连接";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConn.Location = new System.Drawing.Point(943, 8);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(75, 23);
+            this.btnConn.TabIndex = 5;
+            this.btnConn.Text = "连接";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // tbConnStr
+            // 
+            this.tbConnStr.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConnStr.Location = new System.Drawing.Point(90, 10);
+            this.tbConnStr.Name = "tbConnStr";
+            this.tbConnStr.Size = new System.Drawing.Size(847, 21);
+            this.tbConnStr.TabIndex = 4;
+            this.tbConnStr.Text = "Server=ZHANGTAO\\SQLEXPRESS;database=JooWMS;user id=sa;Password=511549108";
+            // 
+            // pnBottom
+            // 
+            this.pnBottom.Controls.Add(this.button1);
+            this.pnBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnBottom.Location = new System.Drawing.Point(0, 446);
+            this.pnBottom.Name = "pnBottom";
+            this.pnBottom.Size = new System.Drawing.Size(1029, 35);
+            this.pnBottom.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 446);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(948, 7);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "生成";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // gbCenter
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
+            this.gbCenter.Controls.Add(this.spContent);
+            this.gbCenter.Controls.Add(this.pnTitle);
+            this.gbCenter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbCenter.Location = new System.Drawing.Point(0, 40);
+            this.gbCenter.Name = "gbCenter";
+            this.gbCenter.Size = new System.Drawing.Size(1029, 406);
+            this.gbCenter.TabIndex = 3;
+            this.gbCenter.TabStop = false;
             // 
-            // Form1
+            // pnTitle
+            // 
+            this.pnTitle.Controls.Add(this.cbDb);
+            this.pnTitle.Controls.Add(this.label1);
+            this.pnTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTitle.Location = new System.Drawing.Point(3, 17);
+            this.pnTitle.Name = "pnTitle";
+            this.pnTitle.Size = new System.Drawing.Size(1023, 36);
+            this.pnTitle.TabIndex = 0;
+            // 
+            // spContent
+            // 
+            this.spContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spContent.Location = new System.Drawing.Point(3, 53);
+            this.spContent.Name = "spContent";
+            // 
+            // spContent.Panel1
+            // 
+            this.spContent.Panel1.Controls.Add(this.lvTable);
+            // 
+            // spContent.Panel2
+            // 
+            this.spContent.Panel2.Controls.Add(this.tabMain);
+            this.spContent.Size = new System.Drawing.Size(1023, 350);
+            this.spContent.SplitterDistance = 200;
+            this.spContent.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择数据库";
+            // 
+            // cbDb
+            // 
+            this.cbDb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDb.FormattingEnabled = true;
+            this.cbDb.Location = new System.Drawing.Point(87, 9);
+            this.cbDb.Name = "cbDb";
+            this.cbDb.Size = new System.Drawing.Size(180, 20);
+            this.cbDb.TabIndex = 1;
+            this.cbDb.SelectedIndexChanged += new System.EventHandler(this.cbDb_SelectedIndexChanged);
+            // 
+            // lvTable
+            // 
+            this.lvTable.CheckBoxes = true;
+            this.lvTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.TableName});
+            this.lvTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvTable.Location = new System.Drawing.Point(0, 0);
+            this.lvTable.Name = "lvTable";
+            this.lvTable.Size = new System.Drawing.Size(200, 350);
+            this.lvTable.TabIndex = 0;
+            this.lvTable.UseCompatibleStateImageBehavior = false;
+            this.lvTable.View = System.Windows.Forms.View.Details;
+            this.lvTable.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.lvTable_ItemChecked);
+            // 
+            // TableName
+            // 
+            this.TableName.Text = "表名称";
+            this.TableName.Width = 200;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabMain.Location = new System.Drawing.Point(0, 0);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(819, 350);
+            this.tabMain.TabIndex = 0;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1029, 481);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.gbCenter);
+            this.Controls.Add(this.pnBottom);
+            this.Controls.Add(this.pnTop);
+            this.Name = "MainForm";
+            this.Text = "芝麻自动";
+            this.pnTop.ResumeLayout(false);
+            this.pnTop.PerformLayout();
+            this.pnBottom.ResumeLayout(false);
+            this.gbCenter.ResumeLayout(false);
+            this.pnTitle.ResumeLayout(false);
+            this.pnTitle.PerformLayout();
+            this.spContent.Panel1.ResumeLayout(false);
+            this.spContent.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spContent)).EndInit();
+            this.spContent.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Panel pnTop;
+        private System.Windows.Forms.Label lbConnStr;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.TextBox tbConnStr;
+        private System.Windows.Forms.Panel pnBottom;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox gbCenter;
+        private System.Windows.Forms.Panel pnTitle;
+        private System.Windows.Forms.SplitContainer spContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDb;
+        private System.Windows.Forms.ListView lvTable;
+        private System.Windows.Forms.ColumnHeader TableName;
+        private System.Windows.Forms.TabControl tabMain;
     }
 }
--- AutoCoding/MainForm.cs
+++ AutoCoding/MainForm.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace AutoCoding
 {
     public partial class MainForm : Form
     {
+        private SqlConnection Conn = null;
         public MainForm()
         {
             InitializeComponent();
@@ -42,6 +44,169 @@ namespace AutoCoding
             string text = temp.TransformText();
 
             System.IO.File.WriteAllText(tableEntity.TableName+"Entity.cs", text, Encoding.UTF8);
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            string strConn = tbConnStr.Text.Trim();
+            if(!string.IsNullOrEmpty(strConn))
+            {
+                Conn = new SqlConnection(strConn);
+                try
+                {
+                    Conn.Open();
+                    if(Conn.State == ConnectionState.Open)
+                    {
+                        tbConnStr.Enabled = false;
+                        SqlCommand cmd = new SqlCommand();
+                        cmd.Connection = Conn;
+                        cmd.CommandText = @"select name from master..sysdatabases
+                                             where name not in ('master','tempdb','model','msdb')";
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        foreach (DataRow dr in ds.Tables[0].Rows)
+                        {
+                            cbDb.Items.Add(dr[0]);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                MessageBox.Show("请输入数据库连接字符串！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void cbDb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string strDbName = cbDb.SelectedText;
+            try
+            {
+                if(Conn == null)
+                {
+                    string strConn = tbConnStr.Text.Trim();
+                    Conn = new SqlConnection(strConn);
+                }
+                if (Conn.State == ConnectionState.Closed)
+                {
+                    Conn.Open();
+                }
+                if (Conn.State == ConnectionState.Open)
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = Conn;
+                    cmd.CommandText = @"select name from "+ strDbName + "..sysObjects where xtype = 'U' order by name";
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        ListViewItem lvi = new ListViewItem(dr[0].ToString());
+                        lvi.SubItems.Add(dr[0].ToString());
+                        lvTable.Items.Add(lvi);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void lvTable_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            if (!e.Item.Checked) return;
+            TabPage tp = new TabPage(e.Item.Text);
+            DataGridView dgv = new DataGridView();
+            //dgv.Columns.Add("ColumnID", "ID");
+            //dgv.Columns.Add("IsPrimaryKey", "主键");
+            //dgv.Columns.Add("ColumnName", "字段名");
+            //dgv.Columns.Add("ColumnType", "字段类型");
+            //dgv.Columns.Add("IsIdentity", "自增");
+            //dgv.Columns.Add("IsNullable", "可空");
+            //dgv.Columns.Add("ByteLength", "字节");
+            //dgv.Columns.Add("CharLength", "字符长度");
+            //dgv.Columns.Add("Precision", "经度");
+            //dgv.Columns.Add("Scale", "刻度");
+            //dgv.Columns.Add("Remark", "字段说明");
+            dgv.Dock = DockStyle.Fill;
+            tp.Controls.Add(dgv);
+            #region SqlText
+            string sqlText = @"WITH indexCTE AS
+                                (
+                                    SELECT 
+                                    ic.column_id,
+                                    ic.index_column_id,
+                                    ic.object_id    
+                                    FROM JooWMS.sys.indexes idx
+                                    INNER JOIN JooWMS.sys.index_columns ic ON idx.index_id = ic.index_id AND idx.object_id = ic.object_id
+                                    WHERE  idx.object_id =OBJECT_ID('{0}') AND idx.is_primary_key=1
+                                )
+                                select
+                                colm.column_id ColumnID,
+                                CAST(CASE WHEN indexCTE.column_id IS NULL THEN 0 ELSE 1 END AS BIT) IsPrimaryKey,
+                                colm.name ColumnName,
+                                systype.name ColumnType,
+                                colm.is_identity IsIdentity,
+                                colm.is_nullable IsNullable,
+                                cast(colm.max_length as int) ByteLength,
+                                (
+                                    case 
+                                        when systype.name='nvarchar' and colm.max_length>0 then colm.max_length/2 
+                                        when systype.name='nchar' and colm.max_length>0 then colm.max_length/2
+                                        when systype.name='ntext' and colm.max_length>0 then colm.max_length/2 
+                                        else colm.max_length
+                                    end
+                                ) CharLength,
+                                cast(colm.precision as int) Precision,
+                                cast(colm.scale as int) Scale,
+                                prop.value Remark
+                                from JooWMS.sys.columns colm
+                                inner join JooWMS.sys.types systype on colm.system_type_id=systype.system_type_id and colm.user_type_id=systype.user_type_id
+                                left join JooWMS.sys.extended_properties prop on colm.object_id=prop.major_id and colm.column_id=prop.minor_id
+                                LEFT JOIN indexCTE ON colm.column_id=indexCTE.column_id AND colm.object_id=indexCTE.object_id                                        
+                                where colm.object_id=OBJECT_ID('{0}')
+                                order by colm.column_id";
+            sqlText = string.Format(sqlText, cbDb.Text + ".dbo." + e.Item.Text);
+            #endregion
+
+            try
+            {
+                if (Conn == null)
+                {
+                    string strConn = tbConnStr.Text.Trim();
+                    Conn = new SqlConnection(strConn);
+                }
+                if (Conn.State == ConnectionState.Closed)
+                {
+                    Conn.Open();
+                }
+                if (Conn.State == ConnectionState.Open)
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = Conn;
+                    cmd.CommandText = sqlText;
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    dgv.DataSource = ds.Tables[0];
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            tabMain.TabPages.Add(tp);
         }
     }
 }
